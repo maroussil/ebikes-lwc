ca8bdbd11617299ceabdd8dafae5270f765face8
Marc-André Roussil <marcandre.roussil@gmail.com>
2019-03-07 14:59:04 -0500

    Completed trailhead

---
+++ force-app/main/default/lwc/detail/detail.js
@@ -0,0 +1,35 @@
+import { LightningElement, track, api, wire } from 'lwc';
+import { bikes } from 'c/data';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import Id from '@salesforce/user/Id';
+import NAME_FIELD from '@salesforce/schema/User.Name';
+const fields = [NAME_FIELD];
+
+
+export default class Detail extends LightningElement {
+
+    // Ensure changes are reactive when product is updated
+    @track product;
+
+    // Private var to track @api productId
+    _productId = undefined;
+
+    // Use set and get to process the value every time it's
+    // requested while switching between products
+    set productId(value) {
+        this._productId = value;
+        this.product = bikes.find(bike => bike.fields.Id.value === value);
+    }
+    
+    // getter for productId
+    @api get productId(){
+        return this._productId;
+    }
+    
+    userId = Id;
+    @wire(getRecord, { recordId: '$userId', fields })
+    user;
+    get name() {
+        return getFieldValue(this.user.data, NAME_FIELD);
+    }
+}
